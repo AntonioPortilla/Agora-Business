1dfaffe2b997daa04d2611c11aa942d86e824408
JALP <julioantonio.lp@gmail.com>
2021-07-25 23:18:29 -0500

    galeria terminada

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,13 +1,43 @@
 var home = (function () {
-  
+
 
     var menu = function () {
-        
+
+    }
+
+    var sliderHome = function() {
+
+        /*$('#contentSlider').slick({
+            centerMode: true,
+            centerPadding: '60px',
+            slidesToShow: 5,
+            responsive: [
+              {
+                breakpoint: 768,
+                settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 3
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 1
+                }
+              }
+            ]
+          });*/
     }
 
     return {
-        init: function () {            
+        init: function () {
             menu();
+            sliderHome();
         },
     }
 })()
@@ -15,3 +45,19 @@ var home = (function () {
 $(document).ready(function () {
     home.init()
 })
+/*
+$(window).load(function() {
+    $('#contentSlider').EasySlides({
+      'show': 5,
+      'vertical':'middle',
+      'touchevents': false,
+    })
+  });
+*/
+$(window).on('load', function () {
+    $('#contentSlider').EasySlides({
+        'show': 11,
+        'vertical':'middle',
+        'touchevents': false,
+      })
+});
